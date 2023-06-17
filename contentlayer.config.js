938bd78784ae96eb51c351004753c1e4e10a51d7
Rayan Kazi <rayankazi7515@gmail.com>
2023-06-17 10:22:18 -0700

    feat: added nice code blocks

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,4 +1,7 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
+import rehypePrettyCode from "rehype-pretty-code"
+import remarkGfm from "remark-gfm"
 
 /** @type {import('contentlayer/source-files').ComputedFields} */
 const computedFields = {
@@ -51,4 +54,36 @@ export const Post = defineDocumentType(() => ({
 export default makeSource({
   contentDirPath: "./content",
   documentTypes: [Post, Page],
+  mdx: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: "one-dark-pro",
+          onVisitLine(node) {
+            // Prevent lines from collapsing in `display: grid` mode, and allow empty
+            // lines to be copy/pasted
+            if (node.children.length === 0) {
+              node.children = [{ type: "text", value: " " }]
+            }
+          },
+          onVisitHighlightedLine(node) {
+            node.properties.className.push("line--highlighted")
+          },
+          onVisitHighlightedWord(node) {
+            node.properties.className = ["word--highlighted"]
+          },
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          properties: {
+            className: ["anchor"],
+          },
+        },
+      ],
+    ],
+  },
 })
